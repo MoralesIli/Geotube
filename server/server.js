faae2bf69b2e998dae118b4f24cd2c66fbe88a0e
Pedro López <21160689@itoaxaca.edu.mx>
2025-11-03 13:58:23 -0600

    produccion: requier un pool y no solo una conexion

--- server/server.js
+++ server/server.js
@@ -29,25 +29,28 @@ console.log(
 );
 
 // Configuración de la conexión a MySQL
-const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "12345",
-  database: process.env.DB_NAME || "geotube_db",
+const db = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '12345',
+  database: process.env.DB_NAME || 'geotube_db',
   port: parseInt(process.env.DB_PORT),
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 // Conectar a la base de datos
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MySQL:", err.stack);
-    return;
-  }
-  console.log("Connected to MySQL as id " + db.threadId);
-});
+// db.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to MySQL:", err.stack);
+//     return;
+//   }
+//   console.log("Connected to MySQL as id " + db.threadId);
+// });
 
 const allowedOrigins = [
-  "https://deploy-geotube-front.vercel.app", //  fronted en producción
+  "https://geotube-one.vercel.app/", //  fronted en producción
   "http://localhost:3000", // útil para desarrollo local
 ];
 
